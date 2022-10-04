10fa05e1b77e2ca8b23061f9014d9a570dd35f36
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-10-04 15:10:18 -0300

    chore: add initial config component Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,32 @@
+import { ReactComponent as Search } from "assets/icons/search.svg";
+import * as S from "./style";
+
 const Home = () => {
-    return(<div>Componente Home</div>);
-  }
-  
-  export default Home;
+  return <S.Home>
+          <S.HomeContent>
+            <header>
+              <S.HomeHeaderDetails>
+                <div>
+                  <S.HomeHeaderDetailsLogo>Ogait Ashtar</S.HomeHeaderDetailsLogo>
+                  <S.HomeHeaderDetailsDate>Data</S.HomeHeaderDetailsDate>
+                </div>
+                <S.HomeHeaderDetailsSearch>
+                  <Search/>
+                  <input type="text"  placeholder="Procure pela Música"/>
+                </S.HomeHeaderDetailsSearch>
+              </S.HomeHeaderDetails>
+            </header>
+            <div>
+              <S.HomeProductTitle>
+                <b>Músicas</b>
+              </S.HomeProductTitle>
+              <S.HomeProductList>
+                <p>Lista de músicas aki</p>
+              </S.HomeProductList>
+            </div>
+          </S.HomeContent>
+          
+        </S.Home>;
+};
+
+export default Home;
