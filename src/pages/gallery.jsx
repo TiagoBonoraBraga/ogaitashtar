c9e6a43b87b0103e8c028d8127b1f93cbd9c4439
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2024-01-04 19:00:54 -0300

    feat: create gallery page

---
+++ src/pages/gallery.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/templates/Layout";
+import Header from "@/components/organisms/Header";
+
+export default function Gallery() {
+    return (
+        <>
+        <Layout>
+            <Head />
+        </Layout>
+        </>
+    )
+}
