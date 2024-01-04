01caa71cd3e7f2deae6e0fede3cb35d0fec37fb2
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2024-01-03 21:24:34 -0300

    feat: create productions page, add Layout adn Head Components

---
+++ src/pages/productions.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/templates/Layout';
+import Head from '@/components/molecules/Head';
+
+export default function Productions() {
+    return (
+        <>
+        <Layout>
+            <Head />
+        </Layout>
+        </>
+    )
+}
