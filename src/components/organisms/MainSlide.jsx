fa7512767f18109ec7a38b30e1c8c0d33b0720ff
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2024-01-13 08:51:09 -0300

    feat: create MainSLide and add in index.jsx

---
+++ src/components/organisms/MainSlide.jsx
@@ -0,0 +1,7 @@
+export default function MainSlide() {
+    return (
+        <>
+        <h1>SLide</h1>
+        </>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,14 @@ import MainBanner from "@/components/organisms/MainBanner";
 import SoloBanner from "@/components/organisms/soloBanner";
 import MainCard from "@/components/organisms/MainCard";
 import Head from "@/components/molecules/Head";
+import MainSlide from "@/components/organisms/MainSlide";
 
 export default function Home() {
   return (
     <>
     <Layout>
       <Head />
+      <MainSlide />
       <MainBanner />
       <SoloBanner />
       <MainCard />
