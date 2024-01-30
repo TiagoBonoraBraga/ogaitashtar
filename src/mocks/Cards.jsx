e3a1b9fd8384f65d2a9edcb4035972ed87199478
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2024-01-29 21:16:30 -0300

    chore: change image card

--- src/mocks/Cards.jsx
+++ src/mocks/Cards.jsx
@@ -1,19 +1,22 @@
-import miracao from '/public/miracao.jpg'
+import miracao from '/public/miracao.jpg';
+import kafofo from '/public/images/logoKafofo.png';
+import morro from '/public/images/morro.jpg';
+import contato from '/public/images/contatoImage.jpg'
 export const cards = [
     {
-        image: miracao,
+        image: kafofo,
         title: "Conheça minha discografia!",
         description: "Acompanhe todos os lançamento da minha carreira da Psicodelia ao Heavy Metal",
         link: '/productions'
     },
     {
-        image: miracao,
+        image: morro,
         title: "Galeria de fotos diversas!",
         description: "Conheça todas as atividades musicais e esportivas que desenvolvo",
         link: '/gallery'
     },
     {
-        image: miracao,
+        image: contato,
         title: "Entre em contato comigo!",
         description: "Entre em contato comigo, deixe um mensagem, Bora bater um papo!",
         link: '/contact'
