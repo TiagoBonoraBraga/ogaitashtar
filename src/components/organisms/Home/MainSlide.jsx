1df56cbb4af046e313c89ae37aa524dfba71cae5
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2024-02-15 20:05:20 -0300

    feat: add setTimeOut in prevSlide() - MainSlide

--- src/components/organisms/Home/MainSlide.jsx
+++ src/components/organisms/Home/MainSlide.jsx
@@ -22,11 +22,16 @@ export default function MainSlide() {
 
     const [currentIndex, SetCurrentIndex] = useState(0);
 
-    const prevSlide = () => {
-        const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-        SetCurrentIndex(newIndex);
-    }
+    setTimeout(() => {
+        prevSlide()
+      }, "4000");
+
+      const prevSlide = () => {
+          const isFirstSlide = currentIndex === 0;
+          const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+          SetCurrentIndex(newIndex);
+      }
+
     const nextSlide = () => {
         const isLastSlide = currentIndex === slides.length - 1;
         const newIndex = isLastSlide ? 0 : currentIndex + 1;
