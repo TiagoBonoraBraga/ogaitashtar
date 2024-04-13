2e01747bdf0f0e816a7934fc51826c9f5ef9934b
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2024-04-13 14:27:08 -0300

    feat: create ogaitashtar.jsx page

---
+++ src/pages/ogaitashtar.jsx
@@ -0,0 +1,12 @@
+import BackgroundVideo from "@/components/organisms/BackgroundVideo/BackgroudVideo";
+import Layout from "@/components/templates/Layout";
+
+export default function OgaitAshtarPage() {
+    return (
+        <>
+        <Layout>
+            <BackgroundVideo />
+        </Layout>
+        </>
+    )
+}
